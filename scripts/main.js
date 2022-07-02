064a7eec3723f789395cf1a8694c751b58b7492e
wemifwan <absolute.kevin@live.com>
2022-07-02 14:17:47 +0800

    fix js exception

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,12 @@
-﻿function openPage(pageName, elmnt) {
+﻿function openPage(pageName, elmnt, color) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
+        tabcontent[i].style.display = "none";
     }
     tablinks = document.getElementsByClassName("tablink");
     for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].style.backgroundColor = "";
+        tablinks[i].style.backgroundColor = "";
     }
     document.getElementById(pageName).style.display = "block";
     elmnt.style.backgroundColor = color;
@@ -20,21 +20,20 @@ var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight
 
 function scrollToTop() {
     rootElement.scrollTo({
-      top: 0,
-      behavior: "smooth"
+        top: 0,
+        behavior: "smooth"
     })
 }
 
 scrollToTopBtn.addEventListener("click", scrollToTop)
 
 function handleScroll() {
-  var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight
-  if ((rootElement.scrollTop / scrollTotal ) > 0.05 ) {
-    scrollToTopBtn.classList.add("showBtn")
-  } 
-  else {
-    scrollToTopBtn.classList.remove("showBtn")
-  }
+    var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight
+    if ((rootElement.scrollTop / scrollTotal) > 0.05) {
+        scrollToTopBtn.classList.add("showBtn")
+    } else {
+        scrollToTopBtn.classList.remove("showBtn")
+    }
 }
 
 document.addEventListener("scroll", handleScroll)
